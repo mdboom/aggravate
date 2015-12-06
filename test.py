bd90db519ead3da2c4352d440d8b07f0dee25afd
Michael Droettboom <mdboom@gmail.com>
2015-12-06 05:29:09 -0800

    Fix test for new API

--- test.py
+++ test.py
@@ -6,10 +6,15 @@ from matplotlib import pyplot as plt
 
 Z = np.random.rand(12, 12)
 
+mtx = np.zeros((3, 3))
+mtx[0, 0] = 800/12
+mtx[1, 1] = 800/12
+mtx[2, 2] = 1
+
 for i in range(_aggravate._n_interpolation):
     output = np.zeros((800, 800))
 
-    aggravate.resample(Z, output, 800/12, 0, 0, 800/12, interpolation=i)
+    aggravate.resample(Z, output, mtx, interpolation=i)
 
     plt.figimage(output)
     plt.show()
